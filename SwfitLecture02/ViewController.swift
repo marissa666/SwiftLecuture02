cbc0ec964a4d501de295029938ffe79f3a8bb7be
marissa666 <564297098@qq.com>
2018-07-22 10:27:26 +0800

    add name

--- SwfitLecture02/ViewController.swift
+++ SwfitLecture02/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello,Swift")
+        
+        var name:String = "Marissa"
+        let fullName = name+"Zhang"
+        print(fullName)
     }
 
     override func didReceiveMemoryWarning() {
